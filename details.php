72c11c02e12c4a2a8dd166b281df507bc7eb4588
SARA IBRAHIM ALSHDDI <159874482+SarahAlshddi@users.noreply.github.com>
2026-05-06 01:21:32 +0300

    Update details.php

--- details.php
+++ details.php
@@ -29,25 +29,93 @@ if (!$region) {
         <li><a href="gallery.php">معرض المناطق</a></li>
         <li><a href="admin/login.php">دخول المشرف</a></li>
     </ul>
-    <button onclick="toggleMode()" style="margin-right: auto;">🌙</button>
-</nav>
+<button onclick="toggleMode()" class="mode-btn">🌙</button></nav>
 
 <section class="hero">
     <h1><?php echo $region['name']; ?></h1>
 </section>
 
 <section class="details-box">
-    <img src="<?php echo $region['image']; ?>" alt="<?php echo $region['name']; ?>">
 
-    <h2><?php echo $region['city']; ?></h2>
+<img class="main-details-img" src="<?php echo $region['image']; ?>" alt="<?php echo $region['name']; ?>">
+
+<h2><?php echo $region['name']; ?></h2>
+<p class="region-location"><?php echo $region['city']; ?></p>
 
     <h3>معلومات عن المنطقة</h3>
     <p><?php echo $region['description']; ?></p>
 
-    <h3>المعالم</h3>
-    <p><?php echo $region['landmarks']; ?></p>
+    <div class="info-box">
+
+
+<h3>المميزات</h3>
+<ul>
+<?php
+$text = str_replace("،", ",", $region['features']);
+$items = explode(",", $text);
+
+foreach ($items as $item) {
+    $item = trim($item);
+    if ($item !== "") {
+        echo "<li>" . $item . "</li>";
+    }
+}
+?>
+</ul>
+
+<h3>الأنشطة</h3>
+<ul>
+<?php
+$text = str_replace("،", ",", $region['activities']);
+$items = explode(",", $text);
+
+foreach ($items as $item) {
+    $item = trim($item);
+    if ($item !== "") {
+        echo "<li>" . $item . "</li>";
+    }
+}
+?>
+</ul>
+
+<h3>أبرز المعالم</h3>
+<ul>
+<?php
+$text = str_replace("،", ",", $region['landmarks']);
+$items = explode(",", $text);
+
+foreach ($items as $item) {
+    $item = trim($item);
+    if ($item !== "") {
+        echo "<li>" . $item . "</li>";
+    }
+}
+?>
+</ul>
+
+
+    </div>
+
+<div class="details-gallery">
+<?php if (!empty($region['gallery_image1'])): ?>
+    <img src="<?php echo $region['gallery_image1']; ?>" alt="">
+<?php endif; ?>
+
+<?php if (!empty($region['gallery_image2'])): ?>
+    <img src="<?php echo $region['gallery_image2']; ?>" alt="">
+<?php endif; ?>
+
+<?php if (!empty($region['gallery_image3'])): ?>
+    <img src="<?php echo $region['gallery_image3']; ?>" alt="">
+<?php endif; ?>
+
+</div>
+
 </section>
 
 <script src="js/script.js"></script>
 </body>
+<footer class="footer">
+    <p>© اكتشف السعودية - جامعة الملك سعود</p>
+</footer>
 </html>
