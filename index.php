bfd4e66487854699f56dedbea9d1992cceefb3f9
nada homoud <161245560+NadaHm444@users.noreply.github.com>
2026-05-06 22:47:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,67 +4,54 @@
     <meta charset="UTF-8">
     <title>اكتشف السعودية</title>
 <link rel="stylesheet" href="css/style.css">
-
-
 </head>
 
 <body id="body">
-
-<!--Navbar-->
 <nav class="navbar">
-    <h2>🇸🇦 اكتشف السعودية</h2>
-    <ul>
+<h2>🇸🇦 اكتشف السعودية</h2>
+   <ul>
         <li><a href="index.php">الرئيسية</a></li>
         <li><a href="gallery.php">معرض المناطق</a></li>
-        <li><a href="admin/login.php">دخول المشرف</a></li>
-    </ul>
+        <li><a href="admin/login.php">دخول المشرف</a></li></ul>
 <button onclick="toggleMode()" class="mode-btn">🌙</button></nav>
-</nav>
-<!-- 🏠 Home Intro Section -->
+
+    
+ 
+    
+ </nav>
 <section class="home-intro">
-
-
-    <div class="intro-image">
-        <div class="image-overlay">
+<div class="intro-image">
+   <div class="image-overlay">
             <h2>أهلاً بك 👋</h2>
             <p>ابدأ رحلتك لاكتشاف مناطق المملكة</p>
-        </div>
-    </div>
-
-    <div class="intro-text">
+   </div></div>
+   <div class="intro-text">
         <h1>موقع ثقافي تفاعلي للتعريف بالمملكة</h1>
-
-        <p>
-            استكشف مناطق المملكة العربية السعودية، وتعرّف على أهم المعالم السياحية
-            والثقافية في كل منطقة بطريقة ممتعة وسهلة.
-        </p>
-
-        <a href="gallery.php" class="explore-btn">ابدأ الاستكشاف</a>
-    </div>
+        <p>استكشف مناطق المملكة العربية السعودية، وتعرّف على أهم المعالم السياحية
+            والثقافية في كل منطقة بطريقة ممتعة وسهلة.</p>
+<a href="gallery.php" class="explore-btn">ابدأ الاستكشاف</a>
+ </div>
 
 </section>
-
-<!-- 📦 Cards -->
 <section class="cards">
-    <div class="card">
+   <div class="card">
         <h3>🎯 الهدف</h3>
-        <p>تعريف المستخدم بالمناطق والمعالم السياحية</p>
-    </div>
+        <p>تعريف المستخدم بالمناطق والمعالم السياحية</p></div>
 
     <div class="card">
         <h3>📍 المناطق</h3>
-        <p>استكشاف مناطق المملكة بسهولة</p>
-    </div>
+        <p>استكشاف مناطق المملكة بسهولة</p></div>
 
-    <div class="card">
+<div class="card">
         <h3>📖 التفاصيل</h3>
-        <p>عرض معلومات وصور لكل منطقة</p>
-    </div>
+        <p>عرض معلومات وصور لكل منطقة</p></div>
 </section>
 
+    
+    
 <script src="js/script.js"></script>
 </body>
 <footer class="footer">
-    <p>© اكتشف السعودية - جامعة الملك سعود</p>
+    <p>اكتشف السعودية - جامعة الملك سعود</p>
 </footer>
 </html>
