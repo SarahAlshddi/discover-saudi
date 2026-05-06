60fab7e07689d673b842001c7c9579e89d0dec07
nada homoud <161245560+NadaHm444@users.noreply.github.com>
2026-05-06 22:47:49 +0300

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -3,18 +3,21 @@ include "includes/db.php";
 $result = mysqli_query($conn, "SELECT * FROM regions");
 ?>
 
+
+
 <!DOCTYPE html>
 <html lang="ar" dir="rtl">
 <head>
+    
     <meta charset="UTF-8">
     <title>معرض المناطق</title>
 <link rel="stylesheet" href="css/style.css">
 </head>
 
+    
 <body>
-
 <nav class="navbar">
-    <h2>🇸🇦 اكتشف السعودية</h2>
+  <h2>🇸🇦 اكتشف السعودية</h2>
     <ul>
         <li><a href="index.php">الرئيسية</a></li>
         <li><a href="gallery.php">معرض المناطق</a></li>
@@ -26,32 +29,34 @@ $result = mysqli_query($conn, "SELECT * FROM regions");
 <section class="hero">
     <h1>معرض المناطق</h1>
     <p>استكشف أشهر مناطق المملكة العربية السعودية</p>
+    
 </section>
 
 <section class="filter-buttons">
-    <button onclick="filterRegions('all')">الكل</button>
-    <button onclick="filterRegions('تاريخية')">تاريخية</button>
+    <button onclick= "filterRegions('all')">الكل</button>
+    <button onclick= "filterRegions('تاريخية')">تاريخية</button>
     <button onclick="filterRegions('ساحلية')">ساحلية</button>
     <button onclick="filterRegions('حديثة')">حديثة</button>
 </section>
 
+    
 <section class="gallery">
 
-<?php while ($row = mysqli_fetch_assoc($result)) { ?>
-    <div class="region-card" data-category="<?php echo $row['category']; ?>">
+<?php while($row = mysqli_fetch_assoc($result)){ ?>
+    <div class= "region-card" data-category="<?php echo $row['category']; ?>">
         <img src="<?php echo $row['image']; ?>" alt="<?php echo $row['name']; ?>">
 <p class="card-location"><?php echo $row['city']; ?></p>
 <h3><?php echo $row['name']; ?></h3>
-        <p><?php echo $row['description']; ?></p>
+        <p><?php echo $row['description'];?></p>
         <a href="details.php?id=<?php echo $row['id']; ?>">عرض التفاصيل</a>
     </div>
-<?php } ?>
-
+<?php} ?>
 </section>
 
+    
 <script src="js/script.js"></script>
 </body>
 <footer class="footer">
-    <p>© اكتشف السعودية - جامعة الملك سعود</p>
+    <p>اكتشف السعودية - جامعة الملك سعود</p>
 </footer>
 </html>
